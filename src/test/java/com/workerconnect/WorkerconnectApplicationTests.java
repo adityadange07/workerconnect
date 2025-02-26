c36fa97cbc2bf1a781302802d902ed1c8890c477
Aditya Dange <38073262+adityadange07@users.noreply.github.com>
2025-02-26 14:37:18 +0530

    Add files via upload

---
+++ src/test/java/com/workerconnect/WorkerconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workerconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkerconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
